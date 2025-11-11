239c003204b95cf03cb908328fb9d3cb887b61f9
Mohame-Issa <mohamedamrissa@gmail.com>
2025-11-11 09:48:59 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -5,6 +5,27 @@
 #include "Student.h"
 using namespace std;
 //#write your code here
+class course{
+private:
+string courseCode;
+string courseName:
+int maxStudents
+student* students;
+int currentStudents;
+public:
+addstudent(const Student& s){
+  //
+  //
+  //
+}
+displaycourseInfo(){
+  cout<<"The courseCode is: "<< coursecode<<;
+  cout<<"The coursename is: "<< coursename<<;
+  cout<<"The course maxstudents is: "<< maxstudents<<;
+  cout<<"The current amount of students is: " << currentStudents<<;
+}
+
+}
 
 
 
