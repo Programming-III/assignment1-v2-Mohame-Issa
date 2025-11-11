05ceaff0dae467682567fb40977062f3b772a326
Mohame-Issa <mohamedamrissa@gmail.com>
2025-11-11 09:19:44 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -5,8 +5,16 @@
 #include <string>
 #using namespace std;
 //#write student class here
-
-
+class student{
+private:
+int yearLevel;
+string major;
+public: 
+display(){
+cout<<"The students year level is: "<< yearLevel <<;
+cout<<"The students major is: "<< major <<;
+}
+}
 
 
 
